ec2597f433b43ee932fbef9b670b73f007b15977
Jyotika Mittal <jmittal_be23@thapar.edu>
2026-07-10 15:04:31 +0530

    Fixed project ready for deploymet

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
     if (process.env.NODE_ENV !== 'production' || !origin) {
       callback(null, true);
     } else {
-      const allowed = ['https://expense-2-3bmk.onrender.com'];
+      const allowed = ['https://new-expense-1-i04d.onrender.com'];
       if (allowed.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
